68a4bf164f1dcd3dc73c1aa6b484bd2faf05f527
Manu-Madhu <manum66466@gmail.com>
2023-09-25 03:32:17 +0530

    updadation bd replace essue

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 const mongoose = require("mongoose");
-
 const app = require("./app");
 const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config.env" });
 
-// mongoose
-//   .connect(process.env.DATABASE_LOCAL, {})
-//   .then(() => console.log("DB Connection successful"));
-
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
 mongoose
   .connect(DB, {
@@ -21,16 +13,15 @@ mongoose
   })
   .then(() => console.log('DB connection successful!'));
 
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
 
-  const port = process.env.PORT || 3000;
-  const server = app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
   });
-  
-  process.on('unhandledRejection', err => {
-    console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-    console.log(err.name, err.message);
-    server.close(() => {
-      process.exit(1);
-    });
-  });
+});
